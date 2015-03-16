1d33ef071c33bc9acbc21c100758c2acb0f46e28
Andrew Kos <akos@theonion.com>
2015-03-16 17:26:06 -0500

    Use nested dirty model for special coverage so it recognizes changes to query object

--- app/components/special-coverage/special-coverage-factory.js
+++ app/components/special-coverage/special-coverage-factory.js
@@ -4,5 +4,5 @@ angular.module('specialCoverage.factory', [
   'apiServices'
 ])
   .factory('SpecialCoverage', function (restmod) {
-    return restmod.model('special-coverage').mix('DirtyModel', {});
+    return restmod.model('special-coverage').mix('NestedDirtyModel', {});
   });
