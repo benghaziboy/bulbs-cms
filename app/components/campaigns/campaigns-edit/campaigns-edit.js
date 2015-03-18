57886e8895adafeeb959d6c3d4e5735c1f3c9dff
Mike Parent <mparent@umich.edu>
2015-03-18 10:06:11 -0500

    Campaigns: (lint) fixup indents

--- app/components/campaigns/campaigns-edit/campaigns-edit.js
+++ app/components/campaigns/campaigns-edit/campaigns-edit.js
@@ -3,40 +3,45 @@
 angular.module('campaigns.edit', [
   'saveButton',
   'campaigns.factory',
-]).constant('PIXEL_TYPES', [{name: 'Logo',
-                             value: 'Logo'},
-                            {name: 'Detail',
-                             value: 'Detail'}])
+]).constant('PIXEL_TYPES', [
+  {
+    name: 'Logo',
+    value: 'Logo'
+  },
+  {
+    name: 'Detail',
+    value: 'Detail'
+  }])
   .config(function ($routeProvider, routes) {
     $routeProvider
-      .when('/cms/app/campaigns/edit/:id/', {
-        controller: function ($routeParams, $q, $scope, $window, Campaign, PIXEL_TYPES) {
-          // set title
-          $window.document.title = routes.CMS_NAMESPACE + ' | Edit Campaign';
+    .when('/cms/app/campaigns/edit/:id/', {
+      controller: function ($routeParams, $q, $scope, $window, Campaign, PIXEL_TYPES) {
+        // set title
+        $window.document.title = routes.CMS_NAMESPACE + ' | Edit Campaign';
 
-          // populate model for use
-          $scope.model = Campaign.$find($routeParams.id);
+        // populate model for use
+        $scope.model = Campaign.$find($routeParams.id);
 
-          $scope.PIXEL_TYPES = PIXEL_TYPES;
+        $scope.PIXEL_TYPES = PIXEL_TYPES;
 
-          // set up save state function
-          $scope.saveModel = function () {
-            var promise;
+        // set up save state function
+        $scope.saveModel = function () {
+          var promise;
 
-            if ($scope.model) {
-              // have model, use save promise as deferred
-              promise = $scope.model.$save().$asPromise();
-            } else {
-              // no model, this is an error, defer and reject
-              var deferred = $q.defer();
-              deferred.reject();
-              promise = deferred.promise;
-            }
+          if ($scope.model) {
+            // have model, use save promise as deferred
+            promise = $scope.model.$save().$asPromise();
+          } else {
+            // no model, this is an error, defer and reject
+            var deferred = $q.defer();
+            deferred.reject();
+            promise = deferred.promise;
+          }
 
-            return promise;
-          };
-        },
-        templateUrl: routes.COMPONENTS_URL + 'campaigns/campaigns-edit/campaigns-edit.html',
-        reloadOnSearch: false
-      });
+          return promise;
+        };
+      },
+      templateUrl: routes.COMPONENTS_URL + 'campaigns/campaigns-edit/campaigns-edit.html',
+      reloadOnSearch: false
+    });
   });
--- app/components/campaigns/campaigns-list/campaigns-list.js
+++ app/components/campaigns/campaigns-list/campaigns-list.js
@@ -1,24 +1,24 @@
 'use strict';
 
 angular.module('campaigns.list', [
-        'campaigns.factory'
+  'campaigns.factory'
 ])
   .config(function ($routeProvider, routes) {
-        $routeProvider
-            .when('/cms/app/campaigns/', {
-                controller: function ($scope, $window, Campaign) {
-                    // set title
-                    //$window.document.title = routes.CMS
-                    $scope.campaigns = [];
-                    $scope.$retrieveCampaigns = function (filters) {
-                        Campaign.$collection().$search(filters)
-                        .$then(function (data) {
-                            $scope.campaigns = data;
-                        });
-                    };
-                    $scope.$retrieveCampaigns();
-                },
-                templateUrl: routes.COMPONENTS_URL + 'campaigns/campaigns-list/campaigns-list.html',
-                reloadOnSearch: false
-            });
+  $routeProvider
+  .when('/cms/app/campaigns/', {
+    controller: function    ($scope, $window, Campaign) {
+      // set title
+      //$window.document.title = routes.CMS
+      $scope.campaigns = [];
+      $scope.$retrieveCampaigns = function (filters) {
+        Campaign.$collection().$search(filters)
+        .$then(function (data) {
+          $scope.campaigns = data;
+        });
+      };
+      $scope.$retrieveCampaigns();
+    },
+    templateUrl: routes.COMPONENTS_URL + 'campaigns/campaigns-list/campaigns-list.html',
+    reloadOnSearch: false
+  });
   });
