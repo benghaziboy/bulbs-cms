b94ebb8b2bf6882469d94ff5affc4f161e5acafc
Mike Parent <mparent@umich.edu>
2015-03-18 12:15:10 -0500

    Campaign Edit: encode/decode date fields

--- app/components/campaigns/campaigns-factory.js
+++ app/components/campaigns/campaigns-factory.js
@@ -3,6 +3,25 @@
 angular.module('campaigns.factory', [
   'apiServices'
 ])
+  .filter('iso_date_string_to_moment', function() {
+    return function (dateStr) {
+      return moment(dateStr);
+    };
+  })
+  .filter('moment_to_iso_date_string', function() {
+    return function (momentObj) {
+      return momentObj.format();
+    };
+  })
   .factory('Campaign', function (restmod) {
-    return restmod.model('campaign').mix('NestedDirtyModel', {});
+    return restmod.model('campaign').mix('NestedDirtyModel', {
+      end_date: {
+        decode: 'iso_date_string_to_moment',
+        encode: 'moment_to_iso_date_string',
+      },
+      start_date: {
+        decode: 'iso_date_string_to_moment',
+        encode: 'moment_to_iso_date_string',
+      }
+    });
   });
