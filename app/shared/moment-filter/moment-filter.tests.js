502805a684a67efe23f0a2152fc1a46925957014
Mike Parent <mparent@umich.edu>
2015-03-20 13:35:02 -0500

    Campaign: lint

--- app/shared/moment-filter/moment-filter.tests.js
+++ app/shared/moment-filter/moment-filter.tests.js
@@ -20,16 +20,16 @@ describe('Shared: Moment Filter', function () {
       expect($filter('moment')).not.toBeNull();
   }));
 
-  it("moment filter should turn invalid dates into blank strings", inject(function (momentFilter) {
+  it('moment filter should turn invalid dates into blank strings', inject(function (momentFilter) {
       expect(momentFilter('')).toBe('');
       expect(momentFilter('invalid date')).toBe('');
   }));
 
-  it("moment filter should format date strings", inject(function (momentFilter) {
+  it('moment filter should format date strings', inject(function (momentFilter) {
       expect(momentFilter('2015-03-19T18:00:00-05:00', 'MM/DD/YY')).toBe('03/19/15');
   }));
 
-  it("moment filter should format moments into date strings", inject(function (momentFilter) {
+  it('moment filter should format moments into date strings', inject(function (momentFilter) {
       expect(momentFilter(moment('2015-03-19T18:00:00-05:00'), 'MM/DD/YY')).toBe('03/19/15');
   }));
 
