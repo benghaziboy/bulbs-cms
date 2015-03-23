cfc50ea82621be9f8045a8d2a907b302b23d3a3e
Mike Parent <mparent@umich.edu>
2015-03-23 15:35:58 -0500

    Campaigns: Disabled unsupported "Detail" pixel type (for now)

--- app/components/campaigns/campaigns-edit/campaigns-edit-sponsor-pixel/campaigns-edit-sponsor-pixel-directive.js
+++ app/components/campaigns/campaigns-edit/campaigns-edit-sponsor-pixel/campaigns-edit-sponsor-pixel-directive.js
@@ -2,14 +2,16 @@
 
 angular.module('campaigns.edit.sponsorPixel.directive', [
 ]).constant('PIXEL_TYPES', [
-  {
-    name: 'Logo',
-    value: 'Logo'
-  },
-  {
-    name: 'Detail',
-    value: 'Detail'
-  }])
+    {
+      name: 'Logo',
+      value: 'Logo'
+    }
+    // TODO: Add more types (once added to API)
+    //{
+    //  name: 'Detail',
+    //  value: 'Detail'
+    //},
+  ])
   .directive('campaignsEditSponsorPixel', function (routes) {
     return {
       controller: function($scope, PIXEL_TYPES) {
